16eb41003bc8c7a0f7359eecc150ce9e20a48628
Shofyan <pencari.angin1@gmail.com>
2023-05-29 12:09:33 +0700

    init usecase

---
+++ pkg/usecase/user/usecase.go
@@ -0,0 +1,12 @@
+package user
+
+import (
+	"context"
+
+	"gitlab.playcourt.id/dedenurr12/ymirblog/pkg/entity"
+)
+
+func (i *impl) CreateUser(ctx context.Context, newUser entity.User) (entity.User, error) {
+
+	return entity.User{}, nil
+}
